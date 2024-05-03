1b2faad039c9a7d585f520af588c3e3d76544a57
Rusiann <154247135+Rusiann7@users.noreply.github.com>
2024-05-03 09:38:03 +0800

    Add files via upload

---
+++ backend.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+declare -r start='1'
+
+
+echo "Begin?"
+echo "1 for yes and 0 for no: "
+read -r choice
+echo "Initialization Begininng"
+
+sleep 3s
+
+if [ "$choice" -eq  "$start" ]; then
+
+function start1(){
+    cd /opt/lampp || exit
+    sudo xhost + 
+    sudo systemctl start apache2
+    sudo ./xampp start
+    sudo ./manager-linux-x64.run  
+    exit 
+}
+start1
+
+else {
+    echo "Error"
+}
+fi
